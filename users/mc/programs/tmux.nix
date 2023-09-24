0b3fa545a4b1834dbd578b090db4def7d5502751
mcookly <mc@origen.local>
2023-09-24 00:27:33 -0400

    Let tmux use fish, not bash

--- users/mc/programs/tmux.nix
+++ users/mc/programs/tmux.nix
@@ -6,7 +6,6 @@
   shortcut = "a";
 
   extraConfig = ''
-    set-option -g default-command bash # I believe this is the most efficient way.
     source $XDG_CONFIG_HOME/tmux/themes/base16_one_dark.conf
   '';
 }
