1464dbc17a1074761bf74303045c41538ef62bdf
mcookly <mc@origen.local>
2023-09-25 16:33:08 -0400

    Change tmux theme

--- users/mc/home.nix
+++ users/mc/home.nix
@@ -71,5 +71,17 @@
     clock24 = true;
     escapeTime = 0;
     shortcut = "a";
+    extraConfig = ''
+
+      # Minimal Tmux Status
+      # https://github.com/niksingh710/minimal-tmux-status
+      set-option -g status-position "bottom"
+      set-option -g status-style bg=default,fg=default
+      set-option -g status-justify centre
+      set-option -g status-left '#[bg=default,fg=default,bold]#{?client_prefix,,  tmux  }#[bg=#698DDA,fg=black,bold]#{?client_prefix,  tmux  ,}'
+      set-option -g status-right '#S'
+      set-option -g window-status-format ' #I:#W '
+      set-option -g window-status-current-format '#[bg=#698DDA,fg=black] #I:#W#{?window_zoomed_flag,  , }'
+    '';
   };
 }
